a37fd0e69c54fa7715e1eea4a52dc97fa303ad69
Juan Tuñon <primerproyecto@gmail.com>
2023-06-29 19:05:19 +0200

    tristes avances :-)

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,30 +4,21 @@ import { Container } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
-import { Navigate } from "react-router-dom";
-
 // DE PEDRO
 
 import { registerUser } from "../services/user.service";
 
 const Register = () => {
-  // SETEO LA INFORMACION QUE LE MANDO A BACK
-  /*   const [user, setUser] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [rol, setRol] = useState("User");
-  const [image, setImage] = useState(""); */
-
   const navigate = useNavigate();
 
   // DE PEDRO
   const { register, handleSubmit } = useForm();
   const [res, setRes] = useState({});
   const [send, setSend] = useState(false);
+
+  //FUNCION DE MANEJO DEL FORMULARIO
   const formSubmit = async (formData) => {
     const inputFile = document.getElementById("file-upload").files;
-
-    console.log("ques es formdata", formData);
 
     if (inputFile.length !== 0) {
       // cuando me han hayan puesto una imagen por el input
@@ -50,48 +41,44 @@ const Register = () => {
 
       setSend(true);
       setRes(await registerUser(custonFormData));
-      setSend(false);
-
-      console.log("que diablos es res", res);
-      /*  navigate("/confirmationcode"); */
-
-      const { value: email } = await Swal.fire({
-        title: "Enter your IP address",
-        input: "text",
-        inputLabel: "Your IP address",
-        inputValue: email,
-        showCancelButton: true,
-        inputValidator: (value) => {
-          if (!value) {
-            return "You need to write something!";
-          }
-        },
-      });
-
-      /* if (ipAddress) {
-        Swal.fire(`Your IP address is ${ipAddress}`);
-      } */
-
-      /* navigate("/confirmationcode"); */
-
-      ///! me llamo al servicio
     }
   };
 
   useEffect(() => {
-    /* console.log(res); */
+    if (send) {
+      const kodigodear = async () => {
+        console.log("que diablos es res dentro", res);
+        const { value: confirmationCode } = await Swal.fire({
+          title: "Código de confirmación",
+          input: "text",
+          inputLabel: "Codigo confirmacion",
+          inputPlaceholder: "Cual es tu código de confirmación",
+          inputAttributes: {
+            maxlength: 6,
+            autocapitalize: "off",
+            autocorrect: "off",
+          },
+        });
+
+        if (Number(confirmationCode) === res.data.confirmationCode) {
+          navigate("/canciones");
+          setSend(false);
+        } else {
+          Swal.fire({
+            title: `You fail it`,
+            footer:
+              'Volver a la página de <a href="/users/register">Registro</a>',
+          });
+          setSend(false);
+        }
+      };
+      kodigodear();
+    }
   }, [res]);
 
   useEffect(() => {
     console.log("que es send", send);
   }, [send]);
-
-  /* const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log(
-      `formulario enviado con el usuario ${user} con la contra ${password} y con el rol ${rol} y esta es la imagen que ha subido ${image}`
-    );
-  }; */
 
   return (
     <Container text style={{ marginTop: "7em" }}>
